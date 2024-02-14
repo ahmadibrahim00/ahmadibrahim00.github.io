08eb81b834ef05082f1ac296e574858848dfb147
Ahmad Ibrahim <122996532+ahmadibrahim00@users.noreply.github.com>
2024-02-13 22:34:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('yes').addEventListener('click', function() {
-    document.getElementById('content').innerHTML = '<h1>Bonne réponse!</h1>';
+    document.getElementById('content').innerHTML = '<h1>Bonne réponse!🥳</h1>';
 });
 
 document.getElementById('no').addEventListener('click', function() {
-    document.getElementById('content').innerHTML = '<h1>Mauvaise réponse, veuillez recommencer</h1><button id="retry">Retour</button>';
+    document.getElementById('content').innerHTML = '<h1>Mauvaise réponse🤨, veuillez recommencer...🤔 </h1><button id="retry">Retour</button>';
     document.getElementById('retry').addEventListener('click', function() {
         window.location.reload(); // Cette ligne recharge la page pour revenir à l'état initial
     });
